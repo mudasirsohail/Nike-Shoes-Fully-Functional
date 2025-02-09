77c71277d4a43ec4b1a5b1c858f53482448cb4f0
Mudasir Sohail <159764920+mudasirsohail@users.noreply.github.com>
2025-02-09 18:53:46 +0500

    Update route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY || "", {
-    apiVersion: "2024-12-18.acacia"
+    // apiVersion: "2024-12-18.acacia"
 })
 
 export async function POST(req: NextApiRequest) {
